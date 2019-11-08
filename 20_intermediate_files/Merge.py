a6ac72ce6b9a820bf6c88487773f47ba2b29320b
Sangseok Lee <sangsucki@gmail.com>
2019-11-08 15:31:06 -0500

    merge with population file and ARCOS concatenated file

--- 20_intermediate_files/Merge.py
+++ 20_intermediate_files/Merge.py
@@ -4,7 +4,13 @@ import numpy as np
 
 os.getcwd()
 
-d1=pd.read_csv("/Users/josephlee/estimating-impact-of-opioid-prescription-regulations-team-7/20_intermediate_files/AL_cleaned_grouped.csv")
+d1=pd.read_csv("/Users/josephlee/estimating-impact-of-opioid-prescription-regulations-team-7/10_code/Concatenate/output.csv")
+d2=pd.read_csv("/Users/josephlee/estimating-impact-of-opioid-prescription-regulations-team-7/00_source/population/pop_counties_20062012.csv")
 
+merge=pd.merge(d1, d2, on=['BUYER_STATE','BUYER_COUNTY','year'], how='inner')
+merge.head()
+dr= ['countyfips','STATE','COUNTY','county_name','NAME','variable']
+merge=merge.drop(columns=dr)
+merge.head()
 
-pd.merge(df1, df2, on='Customer_id', how='inner')
+merge.to_csv("merged.csv",index = False)
