1c6c1c683fa84c1bbdf1e92189ebcd0576edbb84
Sangseok Lee <sangsucki@gmail.com>
2019-11-12 17:18:34 -0500

    add mme plot for Florida

--- 30_results/prepost.py
+++ 30_results/prepost.py
@@ -21,11 +21,21 @@ wa_death_comp = ['WA', 'NC', 'MA', 'CO']
 wa_death = df[df['State'].isin(wa_death_comp)]
 wa = df[df['State'] == 'WA']
 
-# FLORIDA
-# diff in diff
+# FLORIDA 
+
+# diff in diff (death)
 (ggplot(fl_death, aes(x ='Year', y='deaths_percap',color = 'State')) + geom_line(alpha=1) + xlim(2003, 2015) + geom_vline(xintercept = 2010))
-# prepost
+
+# prepost (death)
 (ggplot(fl, aes(x ='Year', y='deaths_percap',color = 'State')) + geom_line(alpha=1) + xlim(2003, 2015) + geom_vline(xintercept = 2010))
+
+
+# prepost (shipment)
+(ggplot(fl, aes(x ='Year', y='mme_percap',color = 'State')) + geom_line(alpha=1) + xlim(2003, 2015) + geom_vline(xintercept = 2010))
+
+# diff in diff (shipment)
+(ggplot(fl_mme, aes(x ='Year', y='mme_percap',color = 'State')) + geom_line(alpha=1) + xlim(2003, 2015) + geom_vline(xintercept = 2010))
+
 
 # TEXAS
 # diff in diff
