8c3f3ef2f709a4e02fd998036e161a8fdb29130c
Vanessa Tang <vmtang11@gmail.com>
2019-11-12 21:34:07 -0500

    fixed plot formatting

--- 30_results/prepost.py
+++ 30_results/prepost.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from plotnine import *
 
 df = pd.read_csv('../20_intermediate_files/full_merge.csv')
@@ -21,30 +22,53 @@ wa_death_comp = ['WA', 'NC', 'MA', 'CO']
 wa_death = df[df['State'].isin(wa_death_comp)]
 wa = df[df['State'] == 'WA']
 
+###
+
+# ANALYSIS PLOTS
+
+death_yrs = (2003,2015)
+death_scale = np.arange(2003, 2016, 1)
+
+ship_yrs = (2006, 2013)
+ship_scale = np.arange(2006, 2013, 1)
+
 # FLORIDA 
 
+fl_policy = 2010
+
+# FLORIDA DEATHS
+
 # diff in diff (death)
-(ggplot(fl_death, aes(x ='Year', y='deaths_percap',color = 'State')) + geom_line(alpha=1) + xlim(2003, 2015) + geom_vline(xintercept = 2010))
+(ggplot(fl_death, aes(x ='Year', y='deaths_percap',color = 'State')) + geom_line(alpha=1) + geom_vline(xintercept = fl_policy) + labs(title = 'Florida: Diff-in-Diff Plot of Opioid Deaths') + ylab('Overdose Deaths per Capita') + scale_x_continuous(breaks = death_scale, limits= death_yrs) )
 
 # prepost (death)
-(ggplot(fl, aes(x ='Year', y='deaths_percap',color = 'State')) + geom_line(alpha=1) + xlim(2003, 2015) + geom_vline(xintercept = 2010))
+(ggplot(fl, aes(x ='Year', y='deaths_percap', color = 'State')) + geom_line(alpha=1) + labs(title = 'Florida: Pre-Post Plot of Opioid Deaths') + ylab('Overdose Deaths per Capita') + scale_x_continuous(breaks = death_scale, limits = death_yrs) + geom_vline(xintercept = fl_policy))
 
+# FLORIDA SHIPMENTS
 
 # prepost (shipment)
-(ggplot(fl, aes(x ='Year', y='mme_percap',color = 'State')) + geom_line(alpha=1) + xlim(2003, 2015) + geom_vline(xintercept = 2010))
+(ggplot(fl, aes(x ='Year', y='mme_percap', color = 'State')) + geom_line(alpha=1) + geom_vline(xintercept = fl_policy) + labs(title = 'Florida: Pre-Post Plot of Opioid Shipments') + ylab('Morphine Equivalents per Capita') + scale_x_continuous(breaks = ship_scale, limits = ship_yrs))
 
 # diff in diff (shipment)
-(ggplot(fl_mme, aes(x ='Year', y='mme_percap',color = 'State')) + geom_line(alpha=1) + xlim(2003, 2015) + geom_vline(xintercept = 2010))
+(ggplot(fl_mme, aes(x ='Year', y='mme_percap',color = 'State')) + geom_line(alpha=1) + geom_vline(xintercept = fl_policy) + labs(title = 'Florida: Diff-in-Diff Plot of Opioid Shipments') + ylab('Morphine Equivalents per Capita') + scale_x_continuous(breaks = ship_scale, limits = ship_yrs))
 
 
 # TEXAS
+
+tx_policy = 2007
+
 # diff in diff
-(ggplot(tx_death, aes(x ='Year', y='deaths_percap',color = 'State')) + geom_line(alpha=1) + xlim(2003, 2015) + geom_vline(xintercept = 2007))
+(ggplot(tx_death, aes(x ='Year', y='deaths_percap', color = 'State')) + geom_line(alpha=1) + scale_x_continuous(breaks = death_scale, limits = death_yrs) + geom_vline(xintercept = tx_policy) + labs(title = 'Texas: Diff-in-Diff Plot of Opioid Deaths') + ylab('Overdose Deaths per Capita'))
+
 # prepost
-(ggplot(tx, aes(x ='Year', y='deaths_percap',color = 'State')) + geom_line(alpha=1) + xlim(2003, 2015) + geom_vline(xintercept = 2007))
+(ggplot(tx, aes(x ='Year', y='deaths_percap',color = 'State')) + geom_line(alpha=1) + scale_x_continuous(breaks = death_scale, limits = death_yrs) + geom_vline(xintercept = tx_policy) + labs(title = 'Texas: Pre-Post Plot of Opioid Deaths') + ylab('Overdose Deaths per Capita'))
 
 # WASHINGTON
+
+wa_policy = 2012
+
 # diff in diff
-(ggplot(wa_death, aes(x ='Year', y='deaths_percap',color = 'State')) + geom_line(alpha=1) + xlim(2003, 2015) + geom_vline(xintercept = 2012))
+(ggplot(wa_death, aes(x ='Year', y='deaths_percap',color = 'State')) + geom_line(alpha=1) + scale_x_continuous(breaks = death_scale, limits = death_yrs) + geom_vline(xintercept = wa_policy) + labs(title = 'Washington: Diff-in-Diff Plot of Opioid Deaths') + ylab('Overdose Deaths per Capita'))
+
 # prepost
-(ggplot(wa, aes(x ='Year', y='deaths_percap',color = 'State')) + geom_line(alpha=1) + xlim(2003, 2015) + geom_vline(xintercept = 2012))
+(ggplot(wa, aes(x ='Year', y='deaths_percap',color = 'State')) + geom_line(alpha=1) + scale_x_continuous(breaks = death_scale, limits = death_yrs) + geom_vline(xintercept = wa_policy) + labs(title = 'Washington: Pre-Post Plot of Opioid Deaths') + ylab('Overdose Deaths per Capita'))
